2f2aed9a9bbabd6ee8a7a880d9c121b31bc97c8c
MALIK DINAR <malikdinaras@gmail.com>
2022-11-21 15:59:03 +0530

    'atlas connected'

--- config/connection.js
+++ config/connection.js
@@ -2,18 +2,15 @@ const mongoClient = require('mongodb').MongoClient
 const state={
     db:null
 }
-
+let uri = 'mongodb+srv://malik:123@cluster0.gzoa8sc.mongodb.net/?retryWrites=true&w=majority'
 module.exports.connect=function(done){                    
-    const url = 'mongodb://localhost:27017';
+    const url = uri;
     const dbname = 'shopping';
 
     mongoClient.connect(url,(err,data)=>{
           if(err) return done(err)
           state.db=data.db(dbname)
-
-
           done()
-
       });
 }
 
